1ce1b4c000726ef0abbc3c5d39c12ff5f57f7759
rajratnamaitry <rajratna.maitry@gmail.com>
2025-07-16 01:16:13 +0530

    feat: update tooltip implementation with custom styles and functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatRippleModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTreeModule} from '@angular/material/tree';
+import {DialogModule} from '@angular/cdk/dialog';
 import { MatCustomTooltipModule } from '../../projects/MatCustomTooltip/src/public-api'; // Adjust the path as necessary
 // import { MatCustomTooltipModule } from 'mat-custom-tooltip';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatRippleModule,
+    MatTabsModule,
     MatCustomTooltipModule,
-    MatTooltipModule,
     BrowserModule
   ],
   providers: [],
